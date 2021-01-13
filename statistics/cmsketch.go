212772138255637c28a8c4f66916a5473d34f2ce
yanyongyu <yanyongyu_1@126.com>
2021-01-13 17:20:38 +0800

    :construction: project 4 part 2

--- statistics/cmsketch.go
+++ statistics/cmsketch.go
@@ -14,8 +14,11 @@
 package statistics
 
 import (
+	"math"
 	"reflect"
+	"sort"
 
+	"github.com/cznic/sortutil"
 	"github.com/pingcap/errors"
 	"github.com/pingcap/tidb/sessionctx/stmtctx"
 	"github.com/pingcap/tidb/tablecodec"
@@ -50,6 +53,13 @@ func (c *CMSketch) InsertBytes(bytes []byte) {
 // insertBytesByCount adds the bytes value into the TopN (if value already in TopN) or CM Sketch by delta, this does not updates c.defaultValue.
 func (c *CMSketch) insertBytesByCount(bytes []byte, count uint64) {
 	// TODO: implement the insert method.
+	/* Your code here */
+	h1, h2 := murmur3.Sum128(bytes)
+	c.count += count
+	for i := range c.table {
+		j := (h1 + h2*uint64(i)) % uint64(c.width)
+		c.table[i][j] += uint32(count)
+	}
 }
 
 func (c *CMSketch) queryValue(sc *stmtctx.StatementContext, val types.Datum) (uint64, error) {
@@ -68,7 +78,27 @@ func (c *CMSketch) QueryBytes(d []byte) uint64 {
 
 func (c *CMSketch) queryHashValue(h1, h2 uint64) uint64 {
 	// TODO: implement the query method.
-	return uint64(0)
+	/* Your code here */
+	vals := make([]uint32, c.depth)
+	min := uint32(math.MaxUint32)
+	for i := range c.table {
+		j := (h1 + h2*uint64(i)) % uint64(c.width)
+		if min > c.table[i][j] {
+			min = c.table[i][j]
+		}
+		noise := (c.count - uint64(c.table[i][j])) / (uint64(c.width) - 1)
+		if uint64(c.table[i][j]) < noise {
+			vals[i] = 0
+		} else {
+			vals[i] = c.table[i][j] - uint32(noise)
+		}
+	}
+	sort.Sort(sortutil.Uint32Slice(vals))
+	res := vals[(c.depth-1)/2] + (vals[c.depth/2]-vals[(c.depth-1)/2])/2
+	if res > min {
+		res = min
+	}
+	return uint64(res)
 }
 
 // MergeCMSketch merges two CM Sketch.
